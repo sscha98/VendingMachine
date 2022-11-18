e6f5bfebad744fa2a779735a97343ffb4cc4443c
Greg Sabella <gsabella4@gmail.com>
2022-11-17 19:54:03 -0500

    pushing changes

--- java/src/main/java/com/techelevator/models/Customer.java
+++ java/src/main/java/com/techelevator/models/Customer.java
@@ -13,41 +13,48 @@ public class Customer {
     private BigDecimal remainingMoney;
     private BigDecimal itemPrice;
 
-    public Customer(){}
+    public Customer() {
+    }
 
-    public void addMoney(BigDecimal feedMoney){moneyProvided = moneyProvided.add(feedMoney);}
+    public void addMoney(BigDecimal feedMoney) {
+        moneyProvided = moneyProvided.add(feedMoney);
+    }
 
-    public BigDecimal getMoneyProvided() {return moneyProvided;}
+    public BigDecimal getMoneyProvided() {
+        return moneyProvided;
+    }
 
-    public int getNumberOfItemsPurchased() {return numberOfItemsPurchased;}
+    public int getNumberOfItemsPurchased() {
+        return numberOfItemsPurchased;
+    }
 
-    public BigDecimal getTotalPrice(){
+    public BigDecimal getTotalPrice() {
         BigDecimal sum = BigDecimal.valueOf(0);
 
-        for (Purchasable price : items){
+        for (Purchasable price : items) {
             sum = sum.add(price.getPrice());
         }
         return sum;
     }
 
-    public BigDecimal getRemainingMoney(){
+    public BigDecimal getRemainingMoney() {
         BigDecimal discount = BigDecimal.valueOf(0);
-        if (items.size()%2==0){
-            discount= BigDecimal.valueOf(1);
+        if (items.size() % 2 == 0) {
+            discount = BigDecimal.valueOf(1);
         }
         moneyProvided = moneyProvided.subtract(itemPrice).add(discount);
         return moneyProvided;
     }
 
-    public String getChange(){
+    public String getChange() {
         Map<String, Integer> change = new HashMap<>();
         int dollars = 0;
-        if (moneyProvided.compareTo(new BigDecimal(1))==1 || moneyProvided.compareTo(new BigDecimal(1))==0){
-            dollars =moneyProvided.intValue();
+        if (moneyProvided.compareTo(new BigDecimal(1)) == 1 || moneyProvided.compareTo(new BigDecimal(1)) == 0) {
+            dollars = moneyProvided.intValue();
         }
 
         BigDecimal moneyProvidedCents = moneyProvided.multiply(new BigDecimal(100));
-        int cents = moneyProvidedCents.intValue()- 100*dollars;
+        int cents = moneyProvidedCents.intValue() - 100 * dollars;
         final int QUARTER_VAL = 25;
         final int DIME_VAL = 10;
         final int NICKEL_VAL = 5;
@@ -56,40 +63,36 @@ public class Customer {
         int dimes = 0;
         int nickels = 0;
 
-        while(cents>0){
-<<<<<<< HEAD
-            if (cents/quarterVal>0){
-                quarters = cents/quarterVal;
-                cents = cents -(quarters*quarters);
-            }
-            else if (cents/dimeVal>0){
-                dimes = cents/dimeVal;
-                cents = cents - (dimes*dimeVal);
-            }
-            else if (cents/nickelVal>0) {
-                nickels = cents / nickelVal;
-                cents = cents - (nickels * nickelVal);
-=======
-            if (cents/ QUARTER_VAL >0){
-                quarters = cents/ QUARTER_VAL;
-                cents = cents -(quarters* QUARTER_VAL);
-            } else if (cents/ DIME_VAL >0){
-                dimes = cents/ DIME_VAL;
-                cents = cents - (dimes* DIME_VAL);
-            }else if (cents/ NICKEL_VAL >0) {
+        while (cents > 0) {
+            if (cents / QUARTER_VAL > 0) {
+                quarters = cents / QUARTER_VAL;
+                cents = cents - (quarters * quarters);
+            } else if (cents / DIME_VAL > 0) {
+                dimes = cents / DIME_VAL;
+                cents = cents - (dimes * DIME_VAL);
+            } else if (cents / NICKEL_VAL > 0) {
                 nickels = cents / NICKEL_VAL;
                 cents = cents - (nickels * NICKEL_VAL);
->>>>>>> 4ddc03d2b396a6ea9e541bbf2b0d1e494fedc71a
+
+                if (cents / QUARTER_VAL > 0) {
+                    quarters = cents / QUARTER_VAL;
+                    cents = cents - (quarters * QUARTER_VAL);
+                } else if (cents / DIME_VAL > 0) {
+                    dimes = cents / DIME_VAL;
+                    cents = cents - (dimes * DIME_VAL);
+                } else if (cents / NICKEL_VAL > 0) {
+                    nickels = cents / NICKEL_VAL;
+                    cents = cents - (nickels * NICKEL_VAL);
+                }
             }
         }
-
         return "Your change is: " + dollars + " dollars, " + quarters + " quarters, " + dimes + " dimes, and " +
                 nickels + " nickels";
     }
 
-    public void addItem(Purchasable item){
-        items.add(item);
-        itemPrice = item.getPrice();
+        public void addItem(Purchasable item){
+            items.add(item);
+            itemPrice = item.getPrice();
+        }
     }
 
-}
